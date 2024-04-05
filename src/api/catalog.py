ceebe062dee5dbb393ec0283112c91d28103da57
SeanHershey <shershey@calpoly.edu>
2024-04-05 14:01:04 -0700

    better database.py access

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 import sqlalchemy
 from src import database as db
-import sqlalchemy
-from src import database as db
 
 router = APIRouter()
 
@@ -14,16 +12,14 @@ def get_catalog():
     """
 
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
-        for row in result:
-            print(row)
+        result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
 
-    return [
-        {
-            "sku": "RED_POTION_0",
-            "name": "red potion",
-            "quantity": 1,
-            "price": 50,
-            "potion_type": [100, 0, 0, 0],
-        }
-    ]
+        return [
+            {
+                "sku": "GREEN_POTION_0",
+                "name": "green potion",
+                "quantity": result,
+                "price": 50,
+                "potion_type": [100, 0, 0, 0],
+            }
+        ]
